d5e36b07bf982b0c82d302312d576f3ecafa0eff
Gyles Batara <gyles.batara@live.com>
2017-10-03 10:19:28 -0700

    Finish dropdown menu for links to other pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myMenu").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.menubtn')) {
+
+    var dropdowns = document.getElementsByClassName(".menu-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openMenu = dropdowns[i];
+      if (openMenu.classList.contains('show')) {
+        openMenu.classList.remove('show');
+      }
+    }
+  }
 }
