08cf16da183f327ac2b53f7888264b1a48893ccc
jm <heartfeats@gmail.com>
2017-10-04 13:46:37 -0700

    add bitcoin.css file and BTC investing example

---
+++ js/bitcoin.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#formOne').submit(function(event) {
+    event.preventDefault();
+
+    var principal = document.getElementById("principal").value;
+    var time = document.getElementById("time").value; // no. of years
+    var r = 81.71
+    var result = document.getElementById("result");
+
+    var A = (principal* Math.pow((1 + (r/(100))), (time)));
+
+    result.innerHTML = "Potential value in " + time + " years: $" + A.toFixed(2);
+
+  });
+});
